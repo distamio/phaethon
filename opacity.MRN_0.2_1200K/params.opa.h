5407ef77ab750689e0ac01a51883d9a1cf621bb9
Dimitris <dstamatellos@uclan.ac.uk>
2015-11-11 15:11:54 +0000

    Changed the MNR opacity name (2)

---
+++ opacity.MRN_0.2_1200K/params.opa.h
@@ -0,0 +1,18 @@
+/* opacity.MRN_0.2K_1200 */
+
+/********************** pemit *****************************************/
+#define NFREQ  1000  /* number of frequencies to reemit photons */
+
+/* wavelength limits to reemit photon packets [pemit.c]*/
+
+#define L1     0.1 /* microns */
+#define L2     10000.
+
+
+/*************************** opacity    ********************************/
+
+#define MAX_TEMP  1201.  /* max temperature to calculate mean opacity +1 */
+#define TEMPDIM   6001 /* 1201 */   
+  
+
+#define OPADIM    110 /* dim  >100 of the opacity.dat, if there is one    */
